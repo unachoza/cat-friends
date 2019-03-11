386e7e4a3963feb4f6350e9682d3f4e3f0ac6110
Arianna Choza <unachoza@gmail.com>
2019-03-11 15:22:09 -0400

    these cats

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Card from './Card'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="fi tc">
+       <h1>Hello Ladies</h1> 
+       <Card/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,20 @@
+import React, {Componenet} from 'react'
+
+const Card = () => {
+    return(
+        <div>
+            <img src="https://www.peppercarrot.com/extras/html/2016_cat-generator/avatar.php?seed=me"/>
+            <img src="https://www.peppercarrot.com/extras/html/2016_cat-generator/avatar.php?seed=mom"/>
+            <img src="https://www.peppercarrot.com/extras/html/2016_cat-generator/avatar.php?seed=michelle"/>
+            <img src="https://www.peppercarrot.com/extras/html/2016_cat-generator/avatar.php?seed=her"/>
+            <img src="https://www.peppercarrot.com/extras/html/2016_cat-generator/avatar.php?seed=arianna"/>
+            <img src="https://www.peppercarrot.com/extras/html/2016_cat-generator/avatar.php?seed=arianna"/>
+            
+            <div>
+                <h2>Jane Donner</h2>
+                <p>jane@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+export default Card
