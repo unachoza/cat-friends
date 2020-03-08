554783abadbc8cfa390c1f2ec7517e803250c9e9
arianna choza <unachoza@gmail.com>
2020-03-08 17:07:59 -0400

    added more cat data

---
+++ src/CatData.js
@@ -0,0 +1,122 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'clem.Padberg@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Laura Graham',
+    username: 'Laura',
+    email: 'Laur@april.biz'
+  },
+  {
+    id: 12,
+    name: 'Evan Howell',
+    username: 'Evan',
+    email: 'anna@melissa.tv'
+  },
+  {
+    id: 13,
+    name: 'Claud Bauch',
+    username: 'Claud',
+    email: 'NClaud@yesenia.net'
+  },
+  {
+    id: 14,
+    name: 'Paul Lebsack',
+    username: 'arianne',
+    email: 'paul.OConner@kory.org'
+  },
+  {
+    id: 15,
+    name: 'Chance Dietrich',
+    username: 'Chance',
+    email: 'Chance_Hettinger@annie.ca'
+  },
+  {
+    id: 16,
+    name: 'Mrs. Delilha Schulist',
+    username: 'Delilha',
+    email: 'Delilha@jasper.info'
+  },
+  {
+    id: 17,
+    name: 'Kathrine Weissnat',
+    username: 'Kathrine.Skiles',
+    email: 'Kathrine.Hoeger@billy.biz'
+  },
+  {
+    id: 18,
+    name: 'Nina Runolfsdottir V',
+    username: 'Nina',
+    email: 'nina@rosamond.me'
+  },
+  {
+    id: 19,
+    name: 'Gloria Reichert',
+    username: 'Gloria',
+    email: 'Gloria_McDermott@dana.io'
+  },
+  {
+    id: 110,
+    name: 'Clementina DuBuque',
+    username: 'oriah.Stanton',
+    email: 'Tey.Padberg@karina.biz'
+  }
+];
